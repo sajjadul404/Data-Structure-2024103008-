5fa91b5819e69cace22f9cc95ffb913e3ae1c6a4
sajjadul404 <sajjadul625@gmail.com>
2025-11-27 03:22:52 +0600

    Adjacency Directed

---
+++ 8th Lab/Adjacency_Directed.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, e;
+    cout << "Enter number of vertices: ";
+    cin >> n;
+
+    cout << "Enter number of edges: ";
+    cin >> e;
+
+    int adj[100][100] = {0};
+
+    cout << "Enter edges (u v):" << endl;
+    for(int i = 0; i < e; i++) {
+        int u, v;
+        cin >> u >> v;
+        adj[u][v] = 1;   
+    }
+
+    cout << "\nAdjacency Matrix:\n";
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            cout << adj[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
